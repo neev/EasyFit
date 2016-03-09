e95668719c691238b958765d9f33be62355d428d
neeraja <neeraja.vimal@hotmail.com>
2016-03-09 09:27:09 -0800

    added workout options list

---
+++ app/src/main/java/com/example/android/easyfitness/SwitchButtonListener.java
@@ -0,0 +1,11 @@
+package com.example.android.easyfitness;
+
+/**
+ * Created by neeraja on 3/8/2016.
+ */
+public interface SwitchButtonListener {
+
+    public void onSwitchButtonClickListner(int position,String Workout_desc);
+
+
+}
